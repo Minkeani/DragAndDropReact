9e67ad5a5e069998142a2920e88a9986a5c111ba
minkeani <77203734+Local-Dude@users.noreply.github.com>
2022-04-15 21:37:54 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import  { useState } from "react";
+import './styles/style.css'
+import DragAndDrop from "./components/DragAndDrop";
+
+function App() {
+  
+
+
+  return (
+    <div className="app">
+       <DragAndDrop/>
+    </div>
+  )
+}
+
+export default App;
+
+
